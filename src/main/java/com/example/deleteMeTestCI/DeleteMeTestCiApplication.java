cd147af17bb567cfc37e8c44764437d892f12f86
Glenn Lacco <glenlacc@gmail.com>
2021-08-13 13:43:14 +0800

    init commit

--- src/main/java/com/example/deleteMeTestCI/DeleteMeTestCiApplication.java
+++ src/main/java/com/example/deleteMeTestCI/DeleteMeTestCiApplication.java
@@ -9,6 +9,7 @@ public class DeleteMeTestCiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DeleteMeTestCiApplication.class, args);
 		System.out.println(":::: run DeleteMeTestCiApplication ");
+		System.out.println(":::: run DeleteMeTestCiApplication 2");
 	}
 
 }
