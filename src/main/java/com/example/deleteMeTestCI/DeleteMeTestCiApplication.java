ee397d53351fab7c80e7bf0548fb176ecc780ba5
Glenn Lacco <glenlacc@gmail.com>
2021-08-13 13:24:35 +0800

    init commit

---
+++ src/main/java/com/example/deleteMeTestCI/DeleteMeTestCiApplication.java
@@ -0,0 +1,14 @@
+package com.example.deleteMeTestCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteMeTestCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleteMeTestCiApplication.class, args);
+		System.out.println(":::: run DeleteMeTestCiApplication ");
+	}
+
+}
---
+++ src/test/java/com/example/deleteMeTestCI/DeleteMeTestCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deleteMeTestCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleteMeTestCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
